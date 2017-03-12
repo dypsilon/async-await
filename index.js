338235869dc948bfe5a5955f12106b882a904c63
Dmitriy Navrotskyy <double.ypsilon@gmail.com>
2017-03-12 17:45:54 +0000

    Added an index file.

---
+++ index.js
@@ -0,0 +1,13 @@
+require('./comparison/parallel-few/async-await.js');
+require('./comparison/parallel-few/callbacks.js');
+require('./comparison/parallel-few/promises.js');
+require('./comparison/parallel-many/async-await.js');
+require('./comparison/parallel-many/callbacks.js');
+require('./comparison/parallel-many/promises.js');
+require('./comparison/pipeline/async-await.js');
+require('./comparison/pipeline/callbacks.js');
+require('./comparison/pipeline/promises-chained.js');
+require('./comparison/pipeline/promises-nested.js');
+require('./comparison/sequential-many/async-await.js');
+require('./comparison/sequential-many/callbacks.js');
+require('./comparison/sequential-many/promises.js');
